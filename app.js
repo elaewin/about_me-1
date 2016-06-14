983adf10e728964595cf9fdefd587258f17f375c
Derek Hewitt <derekmhewitt@gmail.com>
2016-06-14 16:39:50 -0700

    working on js, first question done

--- app.js
+++ app.js
@@ -7,13 +7,22 @@
 var userName = prompt('Hi user! Welcome to my About Me page. What is your name?');
 alert ('Welcome ' + userName + ', glad you\'re here.');
 
-var answer1 = prompt('The answer to question 1 is no.').toLowerCase();
+//Q1: Does derek like puppies and licorice?  No, Derek likes ponies and bubblegum.
+var answer1 = prompt('Does Derek like puppies and licorice?').toLowerCase();
 if (answer1 === 'yes' || answer1 === 'y'){
-  alert('Tell user they\'re wrong');
+  alert('Sorry that\'s incorrect, Derek likes ponies and bubblegum.');
 }
 else if(answer1 === 'no' || answer1 === 'n'){
-  alert('Tell the user they\'re right');
+  alert('You\'re right!  Derek actually likes ponies and bubblegum.');
 }
 else{
-  alert('Please enter a Yes or No value for your answer.');
+  alert('Please enter a Yes or No (or Y/N) value for your answer.');
 }
+
+//Q2: Is Derek's favorite color green?  Yes
+
+//Q3: Does Derek like bouldering? No, Derek likes rock and ice climbing.
+
+//Q4: Does Derek like computer gaming?  Yes.
+
+//Q5: Did Derek graduate from High School in 2004?  No, he graduated in 2003.
