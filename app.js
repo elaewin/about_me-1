f81adabb8262da75765c3a10f45e4ac17174388c
Derek Hewitt <derekmhewitt@gmail.com>
2016-06-14 17:18:58 -0700

    debugged and fixed string errors, ready to turn in

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ if (answer1 === 'yes' || answer1 === 'y'){
 }
 else if(answer1 === 'no' || answer1 === 'n'){
   alert('You\'re right!  Derek actually likes ponies and bubblegum.');
-  userAnswerTotal = userAnswerTotal++;
+  userAnswerTotal++;
 }
 else{
   alert('Please enter a Yes or No (or Y/N) value for your answer.');
@@ -29,14 +29,14 @@ else{
 //Q2: Is Derek's favorite color green?  Yes
 var answer2 = prompt('Is Derek\'s favorite color green?').toLowerCase();
 
-console.log('The user answered ' + answer2 + 'to question 2.');
+console.log('The user answered ' + answer2 + ' to question 2.');
 
-if (answer2 === yes || answer2 === y){
-  alert('You\re right, Derek\s favorite color is green!');
-  userAnswerTotal = userAnswerTotal++;
+if (answer2 === 'yes' || answer2 === 'y'){
+  alert('You\'re right, Derek\'s favorite color is green!');
+  userAnswerTotal++;
 }
-else if(answer2 === no || answer2 === n){
-  alert('Sorry you\re wrong, Derek\s favorite color is actually green.');
+else if(answer2 === 'no' || answer2 === 'n'){
+  alert('Sorry you\'re wrong, Derek\'s favorite color is actually green.');
 }
 else{
   alert('Please enter a Yes or No (or Y/N) value for your answer.');
@@ -45,14 +45,14 @@ else{
 //Q3: Does Derek like bouldering? No, Derek likes rock and ice climbing.
 var answer3 = prompt('Does Derek like bouldering?').toLowerCase();
 
-console.log('The user answered ' + answer3 + 'to question 3.');
+console.log('The user answered ' + answer3 + ' to question 3.');
 
-if (answer3 === yes || answer3 === y){
-  alert('Sorry you\re wrong, Derek likes rock and ice climing, not bouldering.');
+if (answer3 === 'yes' || answer3 === 'y'){
+  alert('Sorry you\'re wrong, Derek likes rock and ice climing, not bouldering.');
 }
-else if(answer3 === no || answer3 === n){
-  alert('You\re right, Derek likes rock and ice climbing instead.');
-  userAnswerTotal = userAnswerTotal++;
+else if(answer3 === 'no' || answer3 === 'n'){
+  alert('You\'re right, Derek likes rock and ice climbing instead.');
+  userAnswerTotal++;
 }
 else{
   alert('Please enter a Yes or No (or Y/N) value for your answer.');
@@ -61,14 +61,14 @@ else{
 //Q4: Does Derek like computer gaming?  Yes.
 var answer4 = prompt('Does Derek like computer gaming?').toLowerCase();
 
-console.log('The user answered ' + answer4 + 'to question 4.');
+console.log('The user answered ' + answer4 + ' to question 4.');
 
-if (answer4 === yes || answer4 === y){
-  alert('You\re right, Derek does like computer gaming!');
-  userAnswerTotal = userAnswerTotal++;
+if (answer4 === 'yes' || answer4 === 'y'){
+  alert('You\'re right, Derek does like computer gaming!');
+  userAnswerTotal++;
 }
-else if (answer4 === no || answer4 === n){
-  alert ('Sorry you\re wrong, Derek does like computer gaming.');
+else if (answer4 === 'no' || answer4 === 'n'){
+  alert ('Sorry you\'re wrong, Derek does like computer gaming.');
 }
 else{
   alert('Please enter a Yes or No (or Y/N) value for your answer.');
@@ -77,17 +77,17 @@ else{
 //Q5: Did Derek graduate from High School in 2004?  No, he graduated in 2003.
 var answer5 = prompt('Did Derek graduate from High School in 2004?').toLowerCase();
 
-console.log('The user answered ' + answer5 + 'to question 5.');
+console.log('The user answered ' + answer5 + ' to question 5.');
 
-if (answer5 === yes || answer5 === y){
-  alert('Sorry you\re wrong, Derek graduated from High School in 2003');
+if (answer5 === 'yes' || answer5 === 'y'){
+  alert('Sorry you\'re wrong, Derek graduated from High School in 2003');
 }
-else if (answer5 === no || answer5 === n){
-  alert('You\re right, Derek graduated in 2003!');
-  userAnswerTotal = userAnswerTotal++;
+else if (answer5 === 'no' || answer5 === 'n'){
+  alert('You\'re right, Derek graduated in 2003!');
+  userAnswerTotal++;
 }
 else {
   alert('Please enter a Yes or No (or Y/N) value for your answer.');
 }
 
-alert('Thanks for playing my get-to-know-me game, you got ' + userAnswerTotal + ' questions right out of 5.');
+alert('Thanks for playing my get-to-know-me game ' + userName + ', you got ' + userAnswerTotal + ' questions right out of 5.');
