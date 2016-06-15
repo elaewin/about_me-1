17551bb260c14fae1dcb44dbc99d1d3f783ea9ec
Derek Hewitt <derekmhewitt@gmail.com>
2016-06-15 14:35:20 -0700

    working on js for question 6, broke it, started 7 too

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var userName = prompt('Hi user! Welcome to my About Me page. What is your name?'
 alert ('Welcome ' + userName + ', glad you\'re here.');
 
 //This varaiable stores the # of questions the user gets right and will be displayed at the end
-var userAnswerTotal = 0;
+var userCorrectAnswerTotal = 0;
 
 //Q1: Does derek like puppies and licorice?  No, Derek likes ponies and bubblegum.
 var answer1 = prompt('Does Derek like puppies and licorice?').toLowerCase();
@@ -20,7 +20,7 @@ if (answer1 === 'yes' || answer1 === 'y'){
 }
 else if(answer1 === 'no' || answer1 === 'n'){
   alert('You\'re right!  Derek actually likes ponies and bubblegum.');
-  userAnswerTotal++;
+  userCorrectAnswerTotal++;
 }
 else{
   alert('Please enter a Yes or No (or Y/N) value for your answer.');
@@ -33,7 +33,7 @@ console.log('The user answered ' + answer2 + ' to question 2.');
 
 if (answer2 === 'yes' || answer2 === 'y'){
   alert('You\'re right, Derek\'s favorite color is green!');
-  userAnswerTotal++;
+  userCorrectAnswerTotal++;
 }
 else if(answer2 === 'no' || answer2 === 'n'){
   alert('Sorry you\'re wrong, Derek\'s favorite color is actually green.');
@@ -52,7 +52,7 @@ if (answer3 === 'yes' || answer3 === 'y'){
 }
 else if(answer3 === 'no' || answer3 === 'n'){
   alert('You\'re right, Derek likes rock and ice climbing instead.');
-  userAnswerTotal++;
+  userCorrectAnswerTotal++;
 }
 else{
   alert('Please enter a Yes or No (or Y/N) value for your answer.');
@@ -65,7 +65,7 @@ console.log('The user answered ' + answer4 + ' to question 4.');
 
 if (answer4 === 'yes' || answer4 === 'y'){
   alert('You\'re right, Derek does like computer gaming!');
-  userAnswerTotal++;
+  userCorrectAnswerTotal++;
 }
 else if (answer4 === 'no' || answer4 === 'n'){
   alert ('Sorry you\'re wrong, Derek does like computer gaming.');
@@ -84,40 +84,49 @@ if (answer5 === 'yes' || answer5 === 'y'){
 }
 else if (answer5 === 'no' || answer5 === 'n'){
   alert('You\'re right, Derek graduated in 2003!');
-  userAnswerTotal++;
+  userCorrectAnswerTotal++;
 }
 else {
   alert('Please enter a Yes or No (or Y/N) value for your answer.');
 }
 
 //Q6: Guess at what age Derek got his first compuer of his own?  Answer: 12.  User gets 4 guesses and the question should respond with "too high" or "too low" to the user's guesses so they can narrow in on the right number.
-var answer6 = parseInt(prompt('At what age did Derek get his first computer of his own? He\'s 33 now so guess a number between 0 and 33, you have 4 tries to get the right answer!'));
 var question6CorrectAnswer = 12;
 
-//i = user guess count
-while (i < 4){
-  //If guess right tell the user so
-  if (answer6 === question6CorrectAnswer){
-    i++;
-    alert('You\'re right, congratulations!  And it only took you ' + i + ' guesses!');
+//i = number of times the user guessed the answer
+question6GuessCount = -1;
+do {
+  question6GuessCount++;
+while (question6GuessCount < 4){
+  var answer6 = parseInt(prompt('At what age did Derek get his first computer of his own? He\'s 33 now so guess a number between 0 and 33, you have 4 tries to get the right answer!'));
+  //If the user guesses the right answer tell them so
+  if(question6GuessCount === 4){
+    alert('Sorry you\'re out of guesses, you got this question wrong.');
+  }
+  else if (answer6 === question6CorrectAnswer){
+    question6GuessCount++;
+    alert('You\'re right, congratulations!  And it only took you ' + question6GuessCount + ' guesses!');
+    userCorrectAnswerTotal++;
     break;
   }
   //If guess is too high tell the user "too high"
-  else if (answer6 > question6CorrectAnswer && question6CorrectAnswer < 34){
-    i++;
+  else if (answer6 > question6CorrectAnswer && answer6 < 34){
     alert('Close but no cigar, your guess is too high.');
   }
   //If guess is too low tell the user "too low"
-  else if(answer6 < question6CorrectAnswer && question6CorrectAnswer > 0){
-    i++;
+  else if(answer6 < question6CorrectAnswer && answer6 > 0){
     alert('Close but no cigar, your guess is too low.');
   }
   //If we get here the user probably entered something that wasn't a number, tell them so
   else{
-    i++;
     alert('Your guess wasn\'t between 0 and 33 or wasn\'t a number, but you still used up a guess.  Try again!');
   }
 }
 //Q7: Will go here
 
-alert('Thanks for playing my get-to-know-me game ' + userName + ', you got ' + userAnswerTotal + ' questions right out of 5.');
+var question7GuessCount = 0;
+var question7CorrectAnswer = ['Full Throttle','Wild Cherry Pepsi','Red Bull','Doctor Pepper','Mountain Dew','Barq\'s Root Beer'];
+
+//This alert greets the user by name and lets them know how many questions out of the total they got correct
+
+alert('Thanks for playing my get-to-know-me game ' + userName + ', you got ' + userCorrectAnswerTotal + ' questions right out of 5.');
